27252c21a5582fe60941c341b11bace101df56ee
Luis Alberto Donado <luisinodnd@gmail.com>
2023-10-21 11:28:00 -0500

    Category Model Created

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class CategoryModel
+    {
+        [DisplayName("Category Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Category Name")]
+        [Required(ErrorMessage = "Category name is required")]
+        [StringLength(80, MinimumLength = 3, ErrorMessage = "Category name must be " +
+            "between 3 and 80 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Category Description")]
+        [Required(ErrorMessage = "Category Description is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Category Description must" +
+            " be between 3 and 200 characters")]
+        public string Description { get; set; }
+    }
+}
